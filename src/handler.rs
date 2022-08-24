d18054c10119d37d983d6b1b0b268215d82b0395
Alex K <aiden.hapless@gmail.com>
2022-08-23 18:51:22 -0600

    FileHandler::process

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,18 @@
+use std::path::{Path, PathBuf};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum HandlerCriteria {
     EverythingElse,
     Extensions(&'static [&'static str]),
 }
 
+pub struct ProcessOutput {
+    pub bytes: Vec<u8>,
+    /// The preferred file extension of the output, if ``None``, the extension will not change.
+    pub preferred_extension: Option<&'static str>,
+}
+
 pub trait FileHandler {
     fn criteria(&self) -> HandlerCriteria;
+    fn process(&self, bytes: &[u8], source_path: &Path) -> Result<ProcessOutput, anyhow::Error>;
 }
